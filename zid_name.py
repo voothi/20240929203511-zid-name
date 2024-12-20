e484ad5ea8ac4eabad852bfa52cf135c4627149f
Denis Novikov <voothi@gmail.com>
2024-12-20 16:11:20 +0100

    refactor(zid_name.py): update comments to English for better readability and maintainability fix(zid_name.py): modify regex to allow hyphens in the processed string to preserve valid formatting

--- zid_name.py
+++ zid_name.py
@@ -9,7 +9,7 @@ def set_clipboard_text(text):
     pyperclip.copy(text)
 
 def replace_german_chars(input_string):
-    # Замена немецких специальных символов
+    # Replacement for German special characters
     replacements = {
         'ä': 'ae', 'ö': 'oe', 'ü': 'ue', 'ß': 'ss', 'ẞ': 'ss',
         'Ä': 'ae', 'Ö': 'oe', 'Ü': 'ue'
@@ -19,16 +19,16 @@ def replace_german_chars(input_string):
     return input_string
 
 def process_string(input_string):
-    # Применяем замены для немецких символов
+    # Apply replacements for German characters
     input_string = replace_german_chars(input_string)
     
-    # Удаляем все символы, кроме букв, цифр и пробелов
-    cleaned_string = re.sub(r'[^a-zA-Zа-яА-ЯёЁ0-9\s]', '', input_string)
+    # Allow hyphens to remain and remove other unwanted characters
+    cleaned_string = re.sub(r'[^a-zA-Zа-яА-ЯёЁ0-9\s-]', '', input_string)
     
-    # Приводим строку к нижнему регистру
+    # Convert the string to lowercase
     cleaned_string = cleaned_string.lower()
     
-    # Заменяем пробелы на знак "-"
+    # Replace spaces with "-"
     processed_string = re.sub(r'\s+', '-', cleaned_string)
     
     return processed_string
@@ -37,15 +37,14 @@ def main():
     parser = argparse.ArgumentParser(description="Process input string or clipboard content: lowercase, replace spaces with '-', remove special characters, handle German umlauts.")
     parser.add_argument("input_string", nargs='?', type=str, help="Input string to process. If not provided, clipboard content will be used.")
     args = parser.parse_args()
-
     if args.input_string is None:
         clipboard_content = get_clipboard_text()
+        # print("Clipboard content before processing:", clipboard_content)  # Debug statement
         output_string = process_string(clipboard_content)
         set_clipboard_text(output_string)
     else:
         output_string = process_string(args.input_string)
         set_clipboard_text(output_string)
-
     print(output_string)
 
 if __name__ == "__main__":
